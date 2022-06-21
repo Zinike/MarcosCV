94ee99fc94e1ce8f2df35f3e7c2baded69593b98
Marcos <marcoszinga_1994@hotmail.com>
2022-06-20 23:40:48 -0300

    Grandes cambios

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+//   FUNCIONES DE MI PAGINA
+
 //   FUNCION MOSTRAR MENU
 document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 
@@ -5,10 +7,11 @@ function mostrar_menu() {
   document.querySelector(".menu").classList.toggle("mostrar_menu");
 }
 
+
 //   FUNCION PARALLAX
 window.onscroll = function() {
   var posicion = window.pageYOffset || document.documentElement.scrollTop;
   var selfie = document.getElementById("selfie");
   var texto = document.getElementById("contenedor");
-  selfie.style.right = posicion * 0.5 + "px";
+  selfie.style.right = posicion * 0.25 + "px";
 }
