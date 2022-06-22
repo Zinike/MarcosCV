441496f13fb42980c4561801603ff80386b4e695
Zinike <Marcoszinga_1994@hotmail.com>
2022-06-21 21:16:16 -0300

    Ufff

--- JS/parallax.js
+++ JS/parallax.js
@@ -1,13 +1,3 @@
-//   FUNCIONES DE MI PAGINA
-
-//   FUNCION MOSTRAR MENU
-document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
-
-function mostrar_menu() {
-  document.querySelector(".menu").classList.toggle("mostrar_menu");
-}
-
-
 //   FUNCION PARALLAX
 window.onscroll = function() {
   var posicion = window.pageYOffset || document.documentElement.scrollTop;
---
+++ index.js
@@ -0,0 +1,6 @@
+//   FUNCIONES DE MI PAGINA
+
+const parallax = require('./JS/parallax.js');
+const menu = require('./JS/menu.js');
+
+console.log(menu)
