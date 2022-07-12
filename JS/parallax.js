a602ec5f0ed1e4078d18d1d5265ba407c5c02dcb
Zinike <Marcoszinga_1994@hotmail.com>
2022-07-12 19:00:33 -0300

    asd

--- JS/parallax.js
+++
@@ -1,7 +0,0 @@
-//   FUNCION PARALLAX
-window.onscroll = function() {
-  var posicion = window.pageYOffset || document.documentElement.scrollTop;
-  var selfie = document.getElementById("selfie");
-  var texto = document.getElementById("contenedor");
-  selfie.style.right = posicion * 0.25 + "px";
-}
